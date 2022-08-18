af00ab1f26dd4c878cc31b01eea9e63e2fcbafaa
savchuckvadim <savchuckvadim@gmail.com>
2022-08-18 13:46:02 +0300

    clear routes

--- routes/api.php
+++ routes/api.php
@@ -34,14 +34,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 
-Route::get('/testauth', function () {
-  $result = Auth::user();
-  return $result;
-});
-
-
-
-
 
 Route::middleware(['auth:sanctum'])->group(function () {
   Route::get('/users', function (Request $request) {
@@ -52,11 +44,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     return $collection;
   });
-  // Route::get('/profile/{userId}', function ($userId) {
-  //   // $user_id = $request->data->userId;
-
-  //   return ProfileController::getProfile($userId);
-  // });
 
 
 
@@ -87,51 +74,7 @@ Route::get('/user/auth', function () {
   ], 200);
 });
 
-// Route::get('/user/auth', function () {
-//   // if (Auth::user()->data) {
-//   //   return Auth::user()->data;
-//   // } else {
-//   //   return false;
-//   // }
-//   return Auth::user();
-// });
 
-
-
-
-
-
-//Follow Unfollow
-
-// Route::get('/follow/{id}', function ($id) {
-//   // $currentUserId =  Auth::user()->id;
-//   return FollowersController::follow(1, $id);
-// });
-
-
-// Route::post('/follow', function (Request $request) {
-//   $currentUserId =  Auth::user()->id;
-//   $followedId = $request->userId;
-//   return FollowersController::follow($currentUserId, $followedId);
-// });
-
-// Route::delete('/follow/{userId}', function (Request $request) {
-//   $currentUserId =  Auth::user()->id;
-//   $followedId = $request->userId;
-//   return FollowersController::unfollow($currentUserId, $followedId);
-// });
-
-
-// Route::put('/profile/aboutme', function (Request $request) {
-//   $aboutMe = $request->aboutMe;
-
-//   return ProfileController::updateAboutMe($aboutMe);
-// });
-// Route::get('/profile/aboutme/{userId}', function ($userId) {
-//   // $user_id = $request->data->userId;
-
-//   return ProfileController::getAboutMe($userId);
-// });
 
 
 Route::get('garavatar/{userId}', function ($userId) {
@@ -170,56 +113,6 @@ Route::get('/link/{offerId}', function ($offerId) {
   return  LinkController::create($offerId);
 });
 
-
-
-
-
-
-///////////////POSTS
-// Route::post('/post', function (Request $request) {
-//   return PostController::newPost($request);
-// });
-// Route::get('/post/{profileId}', function ($profileId) {
-//   // return PostController::getPosts($profileId);
-
-//   $posts = Post::where('profile_id', $profileId)->get();
-//   $paginate = Post::paginate(5);
-//   $collection = new PostCollection($posts);
-
-//   return $collection->values();
-// });
-
-// Route::put('/post', function (Request $request) {
-//   return PostController::updatePost($request);
-// });
-
-// Route::post('/like', function (Request $request) {
-//   $like = new Like;
-//   $like->post_id = $request->postId;
-//   $like->author_id = Auth::user()->id;
-//   $like->save();
-
-//   return response(([
-//     'like' => $like,
-//     'resultCode' => 1
-//   ]
-//   ));
-// });
-
-// Route::delete('/like/{postId}', function ($postId) {
-//   $authUserId = Auth::user()->id;
-//   $postsLikes = Like::where('post_id', $postId);
-//   $like = $postsLikes->where('author_id', $authUserId);
-//   $like->delete();
-//   $result = Like::where('post_id', $postId);
-
-//   return response(([
-//     'removedLike' => $like,
-//     'resultCode' => 1
-//   ]
-//   ));
-// });
-////////////////////
 
 
 
